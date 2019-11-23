6d22d507e6ece729caae616593207fd3eca2f844
Guillaume Pratte <guillaume@guillaumepratte.net>
2019-11-23 11:35:34 -0500

    Update route size monitoring dashboard logic
    Fixes #3.
    
    Also add total footer in route size monitoring table

--- src/page/views.py
+++ src/page/views.py
@@ -31,12 +31,15 @@ class HomeView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
         billable_orders_year = Order.objects.filter(
             status='D',
             delivery_date__year=datetime.today().year).count()
+        route_table = self.calculate_route_table()
         context['active_clients'] = active_clients
         context['pending_clients'] = pending_clients
         context['birthday'] = birthday_clients
         context['billable_orders_month'] = billable_orders
         context['billable_orders_year'] = billable_orders_year
-        context['routes'] = self.calculate_route_table()
+        context['routes'] = route_table
+        context['total_scheduled_by_day'] = self.calculate_total_scheduled_by_day(route_table)
+        context['total_episodic_by_day'] = self.calculate_total_episodic_by_day(route_table)
         return context
 
     def calculate_route_table(self):
@@ -63,24 +66,41 @@ class HomeView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
 
         route_table = []
         for route in routes:
-            defaults = collections.defaultdict(int)
+            episodic_defaults = collections.defaultdict(int)
             schedules = collections.defaultdict(int)
             for client in route.selected_clients:
+                # meals_schedule are only for ongoing clients
                 meals_schedule = dict(client.meals_schedule)
-                meals_default = dict(client.meals_default)
 
                 # For each day, if there's a schedule, count schedule.
-                # Otherwise, count default.
                 for day, _ in DAYS_OF_WEEK:
                     if day in meals_schedule:
                         schedules[day] += meals_schedule[day].get(
                             'main_dish') or 0
-                    else:
-                        defaults[day] += meals_default[day].get(
-                            'main_dish') or 0
 
-            route_table.append((route.name, defaults, schedules))
+                if client.delivery_type == 'E':  # Episodic
+                    meals_default = dict(client.meals_default)
+                    for day, _ in DAYS_OF_WEEK:
+                        episodic_defaults[day] += meals_default[day].get('main_dish') or 0
+
+            route_table.append((route.name, episodic_defaults, schedules))
         return route_table
+
+    def calculate_total_scheduled_by_day(self, route_table):
+        total_scheduled_by_day = {}
+        for day, _ in DAYS_OF_WEEK:
+            total_scheduled_by_day[day] = 0
+            for route_entry in route_table:
+                total_scheduled_by_day[day] += route_entry[2][day]
+        return total_scheduled_by_day
+
+    def calculate_total_episodic_by_day(self, route_table):
+        total_episodic_by_day = {}
+        for day, _ in DAYS_OF_WEEK:
+            total_episodic_by_day[day] = 0
+            for route_entry in route_table:
+                total_episodic_by_day[day] += route_entry[1][day]
+        return total_episodic_by_day
 
 
 def custom_login(request):
