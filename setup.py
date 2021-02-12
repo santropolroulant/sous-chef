7f12c085c69345934b83ac86d817478e9a5e7cb0
Guillaume Pratte <guillaume@guillaumepratte.net>
2021-02-12 11:54:03 -0500

    Add link to archive from menu

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="souschef",
-    version="1.3.2dev2",
+    version="1.3.2",
     license="AGPL-3.0",
     author="Santropol Roulant and Savoir Faire Linux",
     author_email="info@santropolroulant.org",
