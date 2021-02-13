5a7dc99b76a5bf95d02d626098868484c4104a0c
Mohammed Rida Louhibi <rlouhibi@outbox.com>
2021-02-13 13:26:15 -0500

    issue #52 cancellation dates should be red

--- souschef/frontend/js/meal-cancellation.js
+++ souschef/frontend/js/meal-cancellation.js
@@ -11,7 +11,10 @@ $(function() {
     $('#id_cancel_meal_dates').hide();
     $('#cancel_meal_dates').multiDatesPicker(config);
     if($('#id_cancel_meal_dates').val()){
-        $('#cancel_meal_dates').multiDatesPicker('addDates', JSON.parse($('#id_cancel_meal_dates').val()));
+        var existing_dates = JSON.parse($('#id_cancel_meal_dates').val());
+        if(existing_dates.length > 0){
+            $('#cancel_meal_dates').multiDatesPicker('addDates', existing_dates);
+        };
 
     }
     $('#cancel_meal_dates').bind('DOMSubtreeModified', function(){
