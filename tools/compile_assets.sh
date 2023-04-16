3cf7f28e5028994235ab193bbfad8b244aeeced9
Guillaume Pratte <guillaume@guillaumepratte.net>
2023-04-16 12:29:53 -0400

    Add platform argument for macOS M1 users

--- tools/compile_assets.sh
+++ tools/compile_assets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PROJECT_ROOT_DIR="$(dirname $0)/.."
 pushd $PROJECT_ROOT_DIR > /dev/null
-docker run -v $(pwd):/code node:10-buster /code/tools/compile_assets_docker.sh
+docker run --platform linux/amd64 -v $(pwd):/code node:10-buster /code/tools/compile_assets_docker.sh
