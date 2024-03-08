d6ff9adb41bf32be29fc29c4638f48dc41cc7b03
Guillaume Pratte <guillaume@guillaumepratte.net>
2024-03-07 21:20:55 -0500

    Include food prepration in kitchen count: add table at the end (#93)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name="souschef",
-    version="1.4.1dev1",
+    version="1.4.1dev3",
     license="AGPL-3.0",
     author="Santropol Roulant and Savoir Faire Linux",
     author_email="info@santropolroulant.org",
